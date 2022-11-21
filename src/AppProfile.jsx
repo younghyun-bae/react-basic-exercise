3418b5d912855af52edf5d1953d8b2bd6dac8756
Younghyun <yhbae97@gmail.com>
2022-11-21 19:01:02 +0100

    add new icon logic

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -8,7 +8,8 @@ function AppProfile() {
       <Profile
         image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
         name='James Alexander'
-        title='Front-end Developer' 
+        title='Front-end Developer'
+        isNew={true} 
       />
       <Profile 
         image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Profile({ image, name, title }) {
+export default function Profile({ image, name, title, isNew }) {
   return (
     <div className='profile'>
       <img 
@@ -8,10 +8,12 @@ export default function Profile({ image, name, title }) {
         src={image} 
         alt='avatar' 
       />
+      { // isNew가 true인 경우에만 이 UI 요소를 보여줘
+        isNew && <span className='new'>New</span>
+      }
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
-
   );
 }
 
