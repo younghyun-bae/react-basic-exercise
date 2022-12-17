e2315547a92a93c94bae21ac88c5bf750ded1587
Younghyun <yhbae97@gmail.com>
2022-12-17 20:38:01 +0100

    modify Navbar reusable

--- src/component/AppWrap.jsx
+++ src/component/AppWrap.jsx
@@ -3,19 +3,25 @@ import React from 'react';
 export default function AppWrap() {
   return (
     <div>
-      <Navbar />
+      <Navbar>
+          <Avatar 
+            image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
+            name='Bob'
+            size={200}
+          />
+          <p>Hello!</p>
+      </Navbar>
+      <Navbar>
+        <p>Thanks!</p>
+      </Navbar>
     </div>
   );
 }
 
-function Navbar() {
+function Navbar({ children }) {
   return (
     <header style={{ backgroundColor: 'yellow' }}>
-      <Avatar 
-        image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
-        name='Bob'
-        size={200}
-      />
+      {children}
     </header>
   );
 }
