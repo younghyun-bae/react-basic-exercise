4b4614d8703279d1f6903c407c7da835c4dfa157
Younghyun <yhbae97@gmail.com>
2022-12-17 20:22:47 +0100

    manage state by object

--- src/component/AppForm.jsx
+++ src/component/AppForm.jsx
@@ -2,21 +2,34 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function AppForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');  
+  const [form, setForm] = useState({ name: '', email: '' });
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(form);
   };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor='name'>name:</label>
-      <input type='text' id='name' name='name' value={name} onChange={(e) => {
-        setName(e.target.value);
-      }}/>
+      <input 
+        type='text' 
+        id='name' 
+        name='name' 
+        value={form.name} 
+        onChange={handleChange} 
+      />
       <label htmlFor='email'>email:</label>
-      <input type='email' id='email' name='email' value={email} onChange={(e) => {
-        setEmail(e.target.value);
-      }}/>
+      <input 
+        type='email' 
+        id='email' 
+        name='email' 
+        value={form.email} 
+        onChange={handleChange} 
+      />
       <button>submit</button>
     </form>
   );
